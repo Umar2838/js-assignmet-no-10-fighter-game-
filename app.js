df152e7cc3cb7a9072a09180eda5b03c40008a3b
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-05-24 02:15:45 +0500

    jakjhaj

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
+var player1 = document.getElementById('player1');
+var player2 = document.getElementById('player2');
 
-const PLAYER_WIDTH = 50;
-const ARENA_WIDTH = 1000;
 
-let player1Position = 10;
-let player2Position = ARENA_WIDTH - PLAYER_WIDTH - 10;
+var spiderManWidth = 100;
+var backgroundWidth = 1000;
 
-function updatePlayers() {
+var player1Position = 10;
+var player2Position = backgroundWidth - spiderManWidth - 10;
+
+function move() {
   player1.style.left = player1Position + 'px';
   player2.style.left = player2Position + 'px';
 }
 
 document.addEventListener('keydown', function(event) {
-  const key = event.key;
+  var key = event.key;
   if (key === 'a') {
-    
     player1Position -= 10;
+
     if (player1Position < 0) {
-      player1Position = 0;
+      player1Position = 10;
     }
   } else if (key === 'd') {
     player1Position += 10;
-    if (player1Position > ARENA_WIDTH - PLAYER_WIDTH) {
-      player1Position = ARENA_WIDTH - PLAYER_WIDTH;
+    if (player1Position > backgroundWidth - spiderManWidth) {
+      player1Position = backgroundWidth - spiderManWidth;
     }
   } else if (key === 'ArrowLeft') {
     player2Position -= 10;
@@ -32,10 +33,47 @@ document.addEventListener('keydown', function(event) {
     }
   } else if (key === 'ArrowRight') {
     player2Position += 10;
-    if (player2Position > ARENA_WIDTH - PLAYER_WIDTH) {
-      player2Position = ARENA_WIDTH - PLAYER_WIDTH;
+    if (player2Position > backgroundWidth - spiderManWidth) {
+      player2Position = backgroundWidth - spiderManWidth;
     }
   }
 
-  updatePlayers();
+
+  
+document.addEventListener("keyup",function(event){
+
+  var key = event.key;
+ 
+
+ if(key === "q"){
+  // key = true
+  var change = player1.src = "images/iron-shields.gif"
+  change.style.height = "300px"
+  change.style.width = "200px"
+ }
+ else{
+  player1.src = "images/iron-walk.gif"
+ }
+
+})
+
+document.addEventListener("keyup",function(event){
+
+  var key = event.key;
+ 
+
+ if(key === "e"){
+  // key = true
+  var change = player2.src = "images/ironman-shield.gif"
+  change.style.height = "300px"
+  change.style.width = "200px"
+ }
+ else{
+  player2.src = "images/iron-man.gif"
+ }
+
+})
+
+  move();
 });
+
